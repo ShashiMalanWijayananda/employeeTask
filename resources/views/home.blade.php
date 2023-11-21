02798cee4b4e044d6cded4bda66cd1fe4f235862
ShashiMalanWijayananda <shashimalanedu@gmail.com>
2023-11-21 11:05:19 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        @extends('examples.dashboard')
+        </div>
+    </div>
+</div>
+@endsection
